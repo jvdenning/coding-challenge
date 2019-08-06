a57e038ab9fe054773d7535e368d8ef180014625
James Denning <v-jadenn@microsoft.com>
2019-08-06 10:13:27 +0100

    Made LINQ statement from hell more readable

--- ConstructionLine.CodingChallenge/SearchEngine.cs
+++ ConstructionLine.CodingChallenge/SearchEngine.cs
@@ -26,8 +26,19 @@ namespace ConstructionLine.CodingChallenge
             return new SearchResults
             {
                 Shirts = matchingShirts,
-                ColorCounts = matchingShirts.Where(ms => options.Colors.Any(c => c.Id == ms.Color.Id)).GroupBy(s => s.Color).Select(c => new ColorCount{Color = c.Key,Count = c.Count()}).Union(Color.All.Where(c=> options.Colors.All(mc => mc.Id != c.Id) || matchingShirts.All(ms => ms.Color.Id != c.Id)).Select(c => new ColorCount{Color = c})).ToList(),
-                SizeCounts = matchingShirts.Where(ms => options.Sizes.Any(s => s.Id == ms.Size.Id)).GroupBy(s => s.Size).Select(c => new SizeCount { Size = c.Key, Count = c.Count() }).Union(Size.All.Where(s => options.Sizes.All(ms => ms.Id != s.Id) || matchingShirts.All(ms => ms.Size.Id != s.Id)).Select(s => new SizeCount{Size = s})).ToList()
+                ColorCounts = matchingShirts.Where(ms => options.Colors.Any(c => c.Id == ms.Color.Id))
+                                    .GroupBy(s => s.Color)
+                                    .Select(c => new ColorCount{Color = c.Key,Count = c.Count()})
+                                    .Union(
+                                        Color.All.Where(c=> options.Colors.All(mc => mc.Id != c.Id) || matchingShirts.All(ms => ms.Color.Id != c.Id))
+                                            .Select(c => new ColorCount{Color = c}))
+                                    .ToList(),
+                SizeCounts = matchingShirts.Where(ms => options.Sizes.Any(s => s.Id == ms.Size.Id))
+                                    .GroupBy(s => s.Size)
+                                    .Select(c => new SizeCount { Size = c.Key, Count = c.Count() })
+                                    .Union(Size.All.Where(s => options.Sizes.All(ms => ms.Id != s.Id) || matchingShirts.All(ms => ms.Size.Id != s.Id))
+                                        .Select(s => new SizeCount{Size = s}))
+                                    .ToList()
             };
         }
     }
